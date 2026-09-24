242a22ad43c24355873dc96b067f956b6b25f05a
Manik <manik3224.beai24@chitkara.edu.in>
2026-09-24 17:14:09 +0530

    new changes

--- Javascript/Object_Destructuring.js
+++ Javascript/Object_Destructuring.js
@@ -15,3 +15,17 @@ console.log(title,author);
 
 const{pages,publisher="unknown"}=book;
 console.log(publisher) // will be printing unknown instead of undefined
+
+
+console.log("title" in book);
+console.log("publisher" in book);
+console.log(book.hasOwnProperty("title"));
+delete book.pages;
+console.log(book);
+
+console.log(Object.keys(book))
+console.log(Object.values(book))
+console.log(Object.entries(book))
+
+const merged=Object.assign({},{a:1},{b:2});
+console.log(merged);
